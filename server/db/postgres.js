1bde3d5fd2e0856396a3f529bec6b3272434e5c9
Marvin Ho <myuanHo1@gmail.com>
2019-08-10 09:34:20 -0700

    Complete Crud API

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -8,44 +8,13 @@ const pool = new Pool({
   password: ''
 })
 
-const Listing = "CREATE TABLE Listing ( id INTEGER PRIMARY KEY, max_guests INTEGER, cleaning_fee INTEGER, local_tax decimal(10,2), min_stay INTEGER, base_rate decimal(10,2), extra_guest_cap INTEGER, extra_guest_charge INTEGER, currency Varchar(25), star_rating decimal(10,1), review_count INTEGER, roomsReserved INTEGER NULL, sub_listings INTEGER[] NULL, has_children Boolean)"
+const Listing = "CREATE TABLE listing ( id INTEGER PRIMARY KEY, max_guests INTEGER, cleaning_fee INTEGER, local_tax decimal(10,2), min_stay INTEGER, base_rate decimal(10,2), extra_guest_cap INTEGER, extra_guest_charge INTEGER, currency Varchar(25), star_rating decimal(10,1), review_count INTEGER, roomsReserved INTEGER NULL, sub_listings INTEGER[] NULL, has_children Boolean)"
 
-const Reserved = "CREATE TABLE Reserved ( id INTEGER PRIMARY KEY, listing_id INTEGER, dates DATE, FOREIGN KEY (listing_id) REFERENCES Listing(id) )"
+const Reservations = "CREATE TABLE reservations ( id INTEGER PRIMARY KEY, listing_id INTEGER, dates DATE, FOREIGN KEY (listing_id) REFERENCES Listing(id) )"
 
-const Custom_rates = "CREATE TABLE Custom_rates ( id INTEGER PRIMARY KEY, listing_id INTEGER, dates DATE, price decimal(10,2), FOREIGN KEY (listing_id) REFERENCES Listing(id))"
+const Custom_rates = "CREATE TABLE custom_rates ( id INTEGER PRIMARY KEY, listing_id INTEGER, dates DATE, price decimal(10,2), FOREIGN KEY (listing_id) REFERENCES Listing(id))"
 
 
-// pool.query(Listing, (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log('Saved Listing Table')
-//   }
-// })
-
-// pool.query(Listing, (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log('Saved Listing Table')
-//   }
-// })
-
-// pool.query(Reserved, (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log('Saved Reserved Table')
-//   }
-// })
-
-// pool.query(Custom_rates, (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log('Saved Custom Rates')
-//   }
-// })
 
 // COPY table_name FROM '/path_to_csv_file.csv' DELIMITERS ',' CSV;
 
@@ -65,11 +34,11 @@ const Custom_rates = "CREATE TABLE Custom_rates ( id INTEGER PRIMARY KEY, listin
 //   }
 // })
 
-// pool.query(`COPY reserved FROM '/Users/marvinho/Reservation/server/db/reserved.csv' DELIMITERS '|' CSV;`, (err, res) => {
+// pool.query(`COPY reservations FROM '/Users/marvinho/Reservation/server/db/reservations.csv' DELIMITERS '|' CSV;`, (err, res) => {
 //   if (err) {
 //     console.log(err.stack)
 //   } else {
-//     console.log('Saved Reserved Dates')
+//     console.log('Saved reservations Dates')
 //   }
 // })
 
